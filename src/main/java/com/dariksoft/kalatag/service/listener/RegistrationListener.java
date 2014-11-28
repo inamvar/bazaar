c37e0d2aea76c2f3c2d538eee5de61a6e351518e
mhdnamvar <namvar@gmail.com>
2014-11-28 19:51:01 +0400

    add email text to messages.properties

--- src/main/java/com/dariksoft/kalatag/service/listener/RegistrationListener.java
+++ src/main/java/com/dariksoft/kalatag/service/listener/RegistrationListener.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
@@ -19,63 +20,63 @@ import com.dariksoft.kalatag.domain.Person;
 
 @Component("registrationListener")
 public class RegistrationListener {
-	
+
 	private Logger log = LoggerFactory.getLogger(RegistrationListener.class);
-	
+
 	@Autowired
 	private JavaMailSender mailSender;
-	
+
 	@Autowired
 	private MessageSource messageSource;
-	
-	
+
 	public void onMessage(Person person) {
-		
+
 		try {
 			log.info("Registeration: " + person + " registered successfully.");
-			sendEmail(person);		
+			sendEmail(person);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public void sendEmail(Person person) {
-        
-		try {
-	          
-//	         Locale locale = LocaleContextHolder.getLocale();
-//	         Locale locale = new Locale("es_ES");
-			Locale locale = new Locale("ar_AE");
-	          
-	          log.info("locale=" + locale);
-	          
-	          StringBuffer htmlText = new StringBuffer();
-	          htmlText.append("<html><body>");
-	          htmlText.append("<p>" + messageSource.getMessage("email.registration.header", new String[]{person.getFirstName(), person.getLastName()}, locale));
-	          htmlText.append("<p>" + messageSource.getMessage("email.registration.body", new String[]{person.getUsername(), person.getPassword()}, locale));
-	          htmlText.append("<p>" + messageSource.getMessage("email.registration.footer", null, locale));
-	          htmlText.append("</body></html>");
-	         
-	          MimeMessage mimeMessage = mailSender.createMimeMessage();
-	          MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
-	          MimeMultipart multipart = new MimeMultipart("related");
-	          BodyPart messageBodyPart = new MimeBodyPart();
-	          
-	          //add html part
-	          messageBodyPart.setContent(htmlText.toString(), "text/html");
-	          multipart.addBodyPart(messageBodyPart);
-	          
-	          mimeMessage.setContent(multipart);       
-	          helper.setTo(person.getUsername());
-	          helper.setSubject("Kalatag registeration");
-	          mailSender.send(mimeMessage);
-	         log.info("For registeration notification an email to "+ person.getUsername() + " has been sent.");
-	          
-	      } catch (Exception e) {
-	          e.printStackTrace();
-	      }
 
-        
-    }
+	public void sendEmail(Person person) {
+
+		try {
+			Locale locale = LocaleContextHolder.getLocale();
+			log.info("locale from context=" + locale);
+			// locale = new Locale("es_ES");
+			// locale = new Locale("ar_AE");
+			locale = new Locale("fa_IR");
+			log.info("locale=" + locale);
+
+			String[] params = new String[4];
+			params[0] = person.getFirstName();
+			params[1] = person.getLastName();
+			params[2] = person.getUsername();
+			params[3] = person.getPassword();
+
+			String htmlText = messageSource.getMessage("email.user.registration", params, locale);
+
+			MimeMessage mimeMessage = mailSender.createMimeMessage();
+			MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "utf-8");
+			MimeMultipart multipart = new MimeMultipart("related");
+			BodyPart messageBodyPart = new MimeBodyPart();
+
+			// add html part
+			messageBodyPart.setContent(htmlText, "text/html");
+			multipart.addBodyPart(messageBodyPart);
+
+			mimeMessage.setContent(multipart);
+			helper.setTo(person.getUsername());
+			helper.setSubject(messageSource.getMessage("email.user.registration.subject", null, locale));
+			mailSender.send(mimeMessage);
+			log.info("For registeration notification an email to "
+					+ person.getUsername() + " has been sent.");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
